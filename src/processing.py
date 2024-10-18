4319108c99f556effe63627effe146c94d3b1abd
Viktorgild <74834337+Viktorgild@users.noreply.github.com>
2024-10-18 19:25:01 +0300

    True

--- src/processing.py
+++ src/processing.py
@@ -15,7 +15,7 @@ def filter_by_state(list_dict: list, state: str = "EXECUTED") -> list:
     return result
 
 
-def sort_by_date(list_dict: list, order: str = "DESC") -> list:
+def sort_by_date(list_dict: list, order: str = True) -> list:
     """Функция сортирует список словарей по дате."""
     result = sorted(list_dict, key=lambda x: x["date"], reverse=(order == "DESC"))
     return result
